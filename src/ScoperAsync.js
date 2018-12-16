c1558894e5c9ad9e8e17fe34ae303858c3eb7b3b
Kapil Verma <kapv89@gmail.com>
2018-12-17 05:20:07 +0530

    applying async scopes to the table concurrently

--- src/ScoperAsync.js
+++ src/ScoperAsync.js
@@ -4,16 +4,16 @@ class ScoperAsync extends Scoper {
   apply(table, params) {
     const actionableParams = this.actionableParams(params);
 
-    return actionableParams
+    return Promise.all(actionableParams
       .filter(({key}) => this.scopes.has(key))
-      .reduce((chain, {key, val}) => chain.then((t) => {
+      .map(({key, val}) => {
         const scope = this.scopes.get(key);
         if (scope instanceof Scoper) {
-          return scope.run(t, val, key).then((t) => t);
+          return scope.run(table, val, key).then((t) => t);
         } else {
-          return scope.bind(this)(t, val, key).then((t) => t);
+          return scope.bind(this)(table, val, key).then((t) => t);
         }
-      }, Promise.resolve(table)))
+      })).then(() => table)
     ;
   }
 }
